0b09945b7502eae8db05ca939bcf1cd9c22022d2
RoyK <roymaestre@innovati.com.co>
2024-09-30 16:07:30 -0500

    Cleaning testing

--- Assets/_Game/Code/Player/Movement.cs
+++ Assets/_Game/Code/Player/Movement.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
@@ -40,12 +42,17 @@ public class Movement : MonoBehaviour
     public bool isGrounded;
     private float gravity = -9.8f;
     public InputAdapter inputAdapter;
+    public Transform floorDetectorPlane;
+    private Vector3 redPlaneNormal;
+    private Vector3 moveVelocity3;
+    public Transform headPosition;
+    private IEnumerator jumpCoroutine;
 
     private void Start()
     {
         inputAdapter = GetComponent<InputAdapter>();
         acceleration = moveSpeed / maxSpeedTime;
-        inputAdapter.OnJumpDown += StartJump;
+        inputAdapter.OnJumpDown += StartJumpSimple;
         inputAdapter.OnJumpUp += StopJump;
         inputAdapter.OnDash += Dash;
         inputAdapter.ToggleInputs(true);
@@ -58,6 +65,8 @@ public class Movement : MonoBehaviour
         Jumping();
         HandleGravity();
         MoveExecuter();
+        DetectSurface();
+        FloatOnTheFloor();
     }
 
     private void CheckGround()
@@ -81,18 +90,43 @@ public class Movement : MonoBehaviour
     float timeFlyingLeft;
     private bool isDashing;
 
-
-
-    private void StartJump(InputAction.CallbackContext context)
+    private void StartJumpSimple(InputAction.CallbackContext context)
     {
         if (isGrounded || extraJumpsLeft > 0)
         {
             if (Time.time > (jumpStarted + jumpCooldown))
             {
-                isJumping = true;
                 jumpStarted = Time.time;
-                zVelocity = jumpForce * minJumpForceMultiplier;
-                if (!isGrounded) extraJumpsLeft--;
+                if (!isGrounded) { 
+                    extraJumpsLeft--;
+                    isJumping = false;
+                    StopCoroutine(jumpCoroutine);
+                }
+                jumpCoroutine = ExecuteJump(inputAdapter.GetMovement());
+                StartCoroutine(jumpCoroutine);
+            }
+        }
+    }
+
+    private IEnumerator ExecuteJump(Vector2 moveInput)
+    {
+        if (!isJumping)
+        {
+            isJumping = true;
+            zVelocity = jumpForce;
+            Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
+            while ((jumpStarted + maxJumpTime) > Time.time)
+            {
+                zVelocity = Math.Clamp(zVelocity + jumpForce * Time.deltaTime, float.MinValue, jumpForce);
+                Vector3 moveForce = moveDirection * acceleration * moveSpeed * Time.deltaTime;
+                transform.Translate((moveForce + Vector3.up * zVelocity) * Time.deltaTime);
+                yield return null;
+            }
+            while (!isGrounded)
+            {
+                Vector3 moveForce = moveDirection * acceleration * moveSpeed * Time.deltaTime;
+                transform.Translate(moveForce * Time.deltaTime);
+                yield return null;
             }
         }
     }
@@ -103,7 +137,7 @@ public class Movement : MonoBehaviour
 
         if (isJumping && (timeFlyingLeft > 0) && inputAdapter.GetJumpButton())
         {
-            zVelocity = Math.Clamp(zVelocity + jumpForce*Time.deltaTime, float.MinValue, jumpForce);
+            zVelocity = Math.Clamp(zVelocity + jumpForce * Time.deltaTime, float.MinValue, jumpForce);
             //zVelocity = jumpForce;
         }
 
@@ -119,34 +153,39 @@ public class Movement : MonoBehaviour
         timeFlyingLeft = 0;
     }
 
+    private Vector3 VerifyPlaneOfMovement(Vector2 direction)
+    {
+        Vector3 correctedDirection = new Vector3(direction.x, 0, direction.y);
+        if (!IsPlaneParallelToXZ(correctedDirection))
+        {
+            correctedDirection = correctedDirection -
+                Vector3.Dot(correctedDirection, floorDetectorPlane.up) * floorDetectorPlane.up;
+        }
+        return correctedDirection;
+    }
+
     private void Move()
     {
         Vector2 moveInput = inputAdapter.GetMovement();
 
-        if (moveInput != Vector2.zero)
+        if (moveInput != Vector2.zero && isGrounded)
         {
-            Vector2 velocity = moveInput * acceleration * Time.deltaTime + moveVelocity;
-            moveVelocity.x = Mathf.Clamp(velocity.x,
-                -moveSpeed,
-                moveSpeed);
-            moveVelocity.y = Mathf.Clamp(velocity.y,
-                -moveSpeed,
-                moveSpeed);
-            //Vector2.ClampMagnitude(moveVelocity, moveSpeed);
+            moveVelocity3 = VerifyPlaneOfMovement(moveInput) *
+                acceleration * moveSpeed * Time.deltaTime;
+            if (moveVelocity3.magnitude > 20f)
+            {
+                //Debugs the moveVelocity, move input, acceleration, delta time
+                Debug.Log("Move Velocity: " + moveVelocity3);
+                Debug.Log("Move Input: " + moveInput);
+                Debug.Log("Acceleration: " + acceleration);
+                Debug.Log("Delta Time: " + Time.deltaTime);
 
-            if (moveInput.x < inputThreshold && moveInput.x > -inputThreshold)
-            {
-                moveVelocity.x = 0;
-            }
-            if (moveInput.y < inputThreshold && moveInput.y > -inputThreshold)
-            {
-                moveVelocity.y = 0;
             }
         }
         else
         {
-            if(inputAdapter.isInputActive)
-                moveVelocity = Vector2.zero;
+            if (inputAdapter.isInputActive)
+                moveVelocity3 = Vector3.zero;
         }
     }
 
@@ -161,9 +200,8 @@ public class Movement : MonoBehaviour
 
     public void MoveExecuter()
     {
-        transform.Translate(
-            new Vector3(moveVelocity.x, zVelocity, moveVelocity.y) 
-            * Time.deltaTime);
+        Vector3 direction = new Vector3(moveVelocity3.x, zVelocity + moveVelocity3.y, moveVelocity3.z);
+        transform.Translate(direction * Time.deltaTime);
     }
 
     public IEnumerator DashExecuter(Vector2 direction)
@@ -177,7 +215,7 @@ public class Movement : MonoBehaviour
 
             while (Time.time < (startTime + DashTime))
             {
-                moveVelocity = dashVelocity;
+                moveVelocity = VerifyPlaneOfMovement(dashVelocity);
                 transform.Translate(moveVelocity.x, 0, moveVelocity.y);
                 yield return null;
             }
@@ -186,4 +224,71 @@ public class Movement : MonoBehaviour
         }
     }
 
+
+    public void DetectSurface()
+    {
+        Ray ray = new Ray(groundCheck.position, Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 100, groundLayer))
+        {
+            floorDetectorPlane.position = hitInfo.point + Vector3.up * 0.01f;
+            floorDetectorPlane.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            floorDetectorPlane.localScale = new Vector3(1f, 0.01f, 1f);
+        }
+        else
+        {
+            floorDetectorPlane.localScale = new Vector3(0, 0, 0);
+        }
+    }
+
+    bool IsPlaneParallelToXZ(Vector3 normal)
+    {
+        Vector3 xzPlaneNormal = Vector3.up;
+        normal.Normalize();
+        float dotProduct = Vector3.Dot(normal, xzPlaneNormal);
+        return Mathf.Approximately(dotProduct, 1f) || Mathf.Approximately(dotProduct, -1f);
+    }
+
+    public void FloatOnTheFloor()
+    {
+        //Verify that the player is not in the middle of the ground, it uses the transform in the head to shoot a raycast down, if it touches the floor means that the player have to go up until it stops touch the floor with the ray, the ray should be 1.2f long
+        Ray ray = new Ray(headPosition.position, Vector3.down);
+        int counterToAvoidInfiniteLoop = 0;
+        while (Physics.Raycast(ray, out RaycastHit hitInfo, 1.2f, groundLayer))
+        {
+            transform.position = hitInfo.point + Vector3.up * 0.01f;
+            counterToAvoidInfiniteLoop++;
+            if (counterToAvoidInfiniteLoop > 1000)
+            {
+                break;
+            }
+        }
+    }
+
+    private Vector3 lastFramePosition = Vector3.zero;
+
+    private void CheckTeleportationError()
+    {
+        //Check is the player is too far from the place it was in the last frame, if it is, if will debug the coordinates
+
+        if (Vector3.Distance(transform.position, lastFramePosition) > 1f)
+        {
+            Debug.Log("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            //Debugs all important information to check the error
+            Debug.Log("Last Frame Position: " + lastFramePosition);
+            Debug.Log("Current Position: " + transform.position);
+            Debug.Log("Time since start: " + Time.timeSinceLevelLoad);
+            Debug.Log("User Input: " + inputAdapter.GetMovement());
+            Debug.Log("Is Jump Pressed: " + inputAdapter.GetJumpButton());
+            StopPlay();
+            
+        }
+        lastFramePosition = transform.position;
+    }
+
+    void StopPlay()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPaused = true; // Stop Play mode in the editor
+#endif
+    }
 }
