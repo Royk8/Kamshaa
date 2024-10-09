35491179d131b7b04ef7b0f99514b2392b75e7df
Drafj <juanpabloc2000@gmail.com>
2024-10-08 21:34:51 -0500

    mantis core ready

---
+++ Assets/Pablo/Scripts/Bosses/BossActivator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossActivator : MonoBehaviour
+{
+    public GameObject bossToActivate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            bossToActivate.SetActive(true);
+    }
+}
