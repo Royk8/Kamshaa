31500022b0b3b48dea427faacd56ca02aa704240
RoyK <roymaestre@innovati.com.co>
2024-10-09 17:27:02 -0500

    Dash attack, collisions and moooore

---
+++ Assets/_Game/Code/Player/DashAttack.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashAttack : MonoBehaviour
+{
+    Collider collider;
+
+    private void Start()
+    {
+        collider = GetComponent<Collider>();
+    }
+    public void Attack()
+    {
+        collider.enabled = true;
+    }
+
+    internal void StopAttack()
+    {
+        collider.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Print enemy hit when collide with them
+
+        IEnemy enemy = other.GetComponent<IEnemy>();
+        if (enemy != null)
+        {
+            Debug.Log("Enemy Hit");
+            enemy.ReceiveDamage(1);
+        }
+
+    }
+}
