199155356d44db67649fc7ac7641a1fb50139bfe
Giloc Delgado <giloc.delgado@innovati.com.co>
2024-10-11 00:10:59 -0500

    puliendo enemigos

--- Assets/_Game/Code/Giloc/Abstract-classes/Bullet.cs
+++ Assets/_Game/Code/Giloc/Abstract-classes/Bullet.cs
@@ -7,11 +7,21 @@ namespace Giloc
         #region properties
         [SerializeField] protected float linearSpeed;
         [SerializeField] protected float maxLifeTime;
+        [SerializeField] protected float damage;
         #endregion
 
         #region methods
         protected abstract void Move();
         protected abstract void UpdateLifeState();
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                var playerHealth =  other.gameObject.GetComponent<IDamageable>();
+                playerHealth.ReceiveDamage(damage);
+            }
+        }
         #endregion
     }
 }
