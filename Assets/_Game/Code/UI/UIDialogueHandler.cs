014a8863b3c033981c142ae25d8b8ed1bb190742
RoyK <roymaestre@innovati.com.co>
2024-10-11 14:32:51 -0500

    Conversation Messages

--- Assets/_Game/Code/UI/UIDialogueHandler.cs
+++ Assets/_Game/Code/UI/UIDialogueHandler.cs
@@ -9,8 +9,10 @@ public class UIDialogueHandler : MonoBehaviour
     public Text conversationText;
     public Image leftImage;
     public Image rightImage;
+    public float lettersPerSecond = 10f;
     public string conversationId;
     TextLocalizationFinder textLocalizationFinder;
+    public DialogueSpriteMap dialogueSpriteMapScriptable;
 
     private void Start()
     {
@@ -35,23 +37,42 @@ public class UIDialogueHandler : MonoBehaviour
         WaitForSeconds wait = new WaitForSeconds(2f);
 
         foreach (var line in lines)
-        {
-            conversationText.text = line.text;
-            if(line.speaker == "Kaamsha")
+        {            
+            string speaker = line.speaker;
+            if(speaker == "Kamshaá")
             {
                 leftImage.gameObject.SetActive(true);
                 rightImage.gameObject.SetActive(false);
             }
             else
             {
-                //rightImage.sprite = line.sprite;
+                Debug.Log("Speaker: " + speaker);
                 leftImage.gameObject.SetActive(false);
                 rightImage.gameObject.SetActive(true);
+                rightImage.sprite = dialogueSpriteMapScriptable.GetSprite(speaker);
             }
+
+            yield return DisplayDialogueAnimation(line.text);
             yield return wait;
         }
 
+        dialoguePanel.SetActive(false);
+
         yield return null;
+    }
+
+    private IEnumerator DisplayDialogueAnimation(string line)
+    {
+        string currentText = "";
+        line += "E";
+        WaitForSeconds wait = new WaitForSeconds(1f / lettersPerSecond);
+
+        for(int i = 0; i < line.Length; i++)
+        {
+            currentText = line.Substring(0, i) + "<color=#00000000>" + line.Substring(i) + "</color>";
+            conversationText.text = currentText;
+            yield return wait;
+        }
     }
 
 
